3787c2ba2f750bc84a2a42d6d29cbf14d1917fd0
dmi0003 <dmi0003@mix.wvu.edu>
2021-10-21 17:42:48 -0400

    Final push R2 hopefully

--- mpx_core/modules/alarm.h
+++ mpx_core/modules/alarm.h
@@ -1,12 +1,28 @@
 #ifndef _ALARM_H
 #define _ALARM_H
-
+/**
+ *Initializes the process for the alarm process.
+ */
 void initAlarm();
 
+/**
+ Sets the alarm.
+ @param message the message user enters.
+ @param hours the hours needed for timer.
+ @param minutes the minutes needed for timer.
+ @param seconds the seconds needed for timer.
+ */
 void setAlarm();
 
+/**
+ * Checks against the the list of times and runs the appropriate process
+ */
 void checkAlarm();
 
+/**
+ * Deletes the appropriate alarm.
+ * @param id the alarm selected for deletion.
+ */
 void deleteAlarm();
 
 
